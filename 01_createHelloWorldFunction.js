36fe6c617db46750153c11b543b262d1deefddf9
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-07-06 04:18:03 +0530

    code added

---
+++ 01_createHelloWorldFunction.js
@@ -0,0 +1,44 @@
+// Question Link: https://leetcode.com/problems/create-hello-world-function/description/?envType=study-plan-v2&envId=30-days-of-javascript
+// Solution Link: https://leetcode.com/problems/create-hello-world-function/solutions/5424082/easy-javascript-solution/
+
+
+/*
+2667. Create Hello World Function
+
+Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+Example 1:
+Input: args = []
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f(); // "Hello World"
+The function returned by createHelloWorld should always return "Hello World".
+Example 2:
+Input: args = [{},null,42]
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f({}, null, 42); // "Hello World"
+Any arguments could be passed to the function but it should still always return "Hello World".
+ 
+Constraints:
+0 <= args.length <= 10
+*/
+
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
